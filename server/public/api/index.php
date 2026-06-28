3189255650026f886a192ac6b8c7aea34afd3376
Hermes <hermes@nous>
2026-06-28 13:08:53 +0200

    Fix api/index.php _init.php path to parent public directory

--- server/public/api/index.php
+++ server/public/api/index.php
@@ -1,7 +1,7 @@
 <?php
 declare(strict_types=1);
 
-require_once __DIR__ . '/_init.php';
+require_once __DIR__ . '/../_init.php';
 
 require_once INCLUDES_DIR . '/db.php';
 require_once INCLUDES_DIR . '/auth.php';
